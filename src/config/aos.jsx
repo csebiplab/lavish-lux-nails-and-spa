ad0318a960667845ca1ad2a6f1dbf867b782ba67
Kongkon Jowarder <kongkon4545@gmail.com>
2024-10-22 00:15:22 +0600

    aos.css file add

--- src/config/aos.jsx
+++ src/config/aos.jsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { useEffect } from "react";
+import "aos/dist/aos.css";
 
 export default function AosSetup() {
   useEffect(() => {
     import("aos").then((AOS) =>
       AOS.init({
         duration: 1200,
+        once: true,
       })
     );
   }, []);
